26a6feee593ed61d5a6bd207e4ac4e78dcb984d8
Andreas Buttenschoen <andreas@buttenschoen.ca>
2023-04-12 14:10:35 -0700

    Add WIP of setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+
+
+setup(
+name='funpy',
+version='0.1',
+description='funPy',
+author='Andreas Buttenschoen',
+author_email='andreas@buttenschoen.ca',
+url='',
+packages=[],
+package_dir={'funpy': 'funpy'},
+)
+
+
+
